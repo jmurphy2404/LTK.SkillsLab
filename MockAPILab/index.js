eb14635bd0a97e38f7915620508b440b1ab3787d
jmurphy2404 <murf2073@gmail.com>
2023-05-15 20:03:58 -0400

    initial commit of work on CRUD API buildout

--- MockAPILab/index.js
+++ MockAPILab/index.js
@@ -2,19 +2,68 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const isLocal = true;
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
 
-app.get('/', (req, res) => {
-    res.json({
-		message: "✨ 👋🌏 ✨",
-		stage: process.env.NODE_ENV,
+app.use(express.json());
+
+const loanSchema = new schema({
+	loanId: Number,
+	borrowers: [{
+		pairId: Number,
+		firstName: String,
+		lastName: String,
+        phone: String,
+	}],
+	collection: "loanData",
+});
+
+const loanData = mongoose.model('loanData', loanSchema);
+
+//check db connection
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
+
+app.get('/loan', (req, res) => {
+	loanData.find({}, (err, data) => {
+		res.json(data);
+		console.log(data);
+	})
+});
+
+app.get('/loan/:loanId', (req, res) => {
+	loanData.find({loanId: req.params.loanId}, (err, data) => {
+		res.json(data);
+		console.log(data);
+	})
+});
+
+app.post('loan/:loanId', (req, res) => {
+	const newLoan = new loanData({
+		loanId: req.params.loanId,
+        borrowers: [req.body],
+	});
+
+	newLoan.save().then(res.json(newLoan));
+});
+
+app.patch('/loan/:loanId', (req, res) => {
+	loanData.findOneAndUpdate({loanId: req.params.loanId, pairId: req.params.pairId, borrowers: req.body})
+	.then(res.json(`Loan ${loanId} was updated`));
+});
+
+app.patch('/loan/removeBorrower/:loanId', (req, res) => {
+	loanData.findOneAndUpdate({loanId: req.params.loanId, pairId: req.params.pairId})
+	.then(res.json(`Loan ${loanId} was updated`));
+});
+
+app.delete('/loan/deleteLoan/:loanId', (req, res) => {
+	loanData.find( {loanId: req.params.loanId}).then( () => {
+		loanData.deleteOne({loanId: req.params.loanId})
+		.then(res.json(`Loan ${loanId} was deleted`));
 	});
 });
 
-app.get("/ping", (req, res) => {
-	res.json({
-		message: "🏓",
-	});
-});
 
 if (isLocal) {
 	//local host
